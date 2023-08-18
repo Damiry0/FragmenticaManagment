b3c1c2bbf98acb80dcdbfbaafa143fac9ed3b6a5
Damian Ryś <damian.rys0@gmail.com>
2023-08-19 00:00:56 +0200

    Implemented Repository pattern

---
+++ FragmenticaManagmentCore/FragmenticaRepository.cs
@@ -0,0 +1,37 @@
+using FragmenticaManagmentCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClassLibrary1;
+
+public class FragmenticaRepository : IRepository<FragmenticaContext>
+{
+    private readonly DbContext _dbContext;
+    public FragmenticaRepository(DbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    public IQueryable<FragmenticaContext> GetAll()
+    {
+        return _dbContext.Set<FragmenticaContext>();
+    }
+
+    public IQueryable<FragmenticaContext> GetAllAsNoTracking()
+    {
+        return _dbContext.Set<FragmenticaContext>().AsNoTracking();
+    }
+
+    public void Add(FragmenticaContext entity)
+    {
+        _dbContext.Set<FragmenticaContext>().Add(entity);
+    }
+
+    public void Delete(FragmenticaContext entity)
+    {
+        _dbContext.Set<FragmenticaContext>().Remove(entity);
+    }
+
+    public void Save()
+    {
+        _dbContext.SaveChanges();
+    }
+}
