0382d7c62e4bec7f9c78a8fc4303d5e9c2308bd3
Damian Ryś <damian.rys0@gmail.com>
2023-08-23 23:58:04 +0200

    Implemented Error ValueObject

---
+++ FragmenticaManagmentCore/Domain/Primitives/Error.cs
@@ -0,0 +1,23 @@
+namespace FragmenticaManagmentCore.Domain.Primitives;
+
+public sealed class Error : ValueObject
+{
+    public Error(string code, string message)
+    {
+        Code = code;
+        Message = message;
+    }
+    
+    public string Code { get; }
+    public string Message { get; }
+    
+
+    public static implicit operator string(Error error) => error?.Code ?? string.Empty;
+    internal static Error None => new Error(string.Empty, string.Empty);
+    
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Code;
+        yield return Message;
+    }
+}
